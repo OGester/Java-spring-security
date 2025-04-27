8711085b509da3f864b94faa2487c4c04838ca28
Oskar Gester <gester.oskar@gmail.com>
2025-04-27 18:54:40 +0200

    Finalised Security config class

--- src/main/java/Spring/Security/restful/api/config/SecurityConfig.java
+++ src/main/java/Spring/Security/restful/api/config/SecurityConfig.java
@@ -1,9 +1,96 @@
 package Spring.Security.restful.api.config;
 
+import Spring.Security.restful.api.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    // creating the authenticationManager
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    // main config and rules for security filter
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+        // CORS configuration
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                // CSRF, disable in dev
+                // OBS dont forget to enable in production!
+
+                .csrf(csrf -> csrf.disable())
+                // URL rules based on endpoints
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/user/**").hasAnyRole("USER","ADMIN")
+                        .requestMatchers("/auth/**").permitAll()
+                        // any other requests require authentication
+                        .anyRequest().authenticated()
+                )
+                // disable session due to Jwt statelessness
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                // add Jwt filter before standard filter
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                return http.build();
+        }
+        // cors config
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+            CorsConfiguration configuration = new CorsConfiguration()
+
+                    // allow access only from our own client
+                    configuration.setAllowedOrigins(List.of("http://localhost:5050"));
+                    configuration.setAllowedMethods(List.of("GET", "POST", "PUT","PATCH","DELETE", "OPTIONS"));
+                    configuration.setExposedHeaders(List.of("*"));
+                    configuration.setAllowCredentials(true);
+                    configuration.setExposedHeaders(List.of("Set-Cookie"));
+
+                    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                    source.registerCorsConfiguration("/**", configuration);
+                    return source;
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            // determine strength of encoded password
+            return new BCryptPasswordEncoder(12);
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
