dcccc4efeb35a4157803aa06881673f1be7be4db
Oskar Gester <gester.oskar@gmail.com>
2025-04-27 15:29:06 +0200

    Jwt Authentication filter done for now

--- src/main/java/Spring/Security/restful/api/filter/JwtAuthenticationFilter.java
+++ src/main/java/Spring/Security/restful/api/filter/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package Spring.Security.restful.api.filter;
 
 import Spring.Security.restful.api.services.CustomUserDetailsService;
 import Spring.Security.restful.api.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
@@ -9,6 +10,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -32,11 +37,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String Jwt = null;
+        String jwt = null;
 
         String bearerToken = request.getHeader("Authorization");
         if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
-            Jwt = bearerToken.substring(7);
+            jwt = bearerToken.substring(7);
         }
         if(jwtUtil == null && request.getCookies() != null) {
             for (Cookie cookie : request.getCookies()) {
@@ -47,6 +52,29 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             }
         }
 
+        if(jwt != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
+            try {
+                String username = jwtUtil.extractUsername(jwt);
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                if(jwtUtil.validateToken(jwt, userDetails)) {
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities()
+                    );
+
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            } catch (JwtException e) {
+                logger.error("JWT validation failed.", e);
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+        }
+        // continue to next filter in chain
+        filterChain.doFilter(request, response);
     }
 }
