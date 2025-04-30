918453a2e5dbf5fc857e7f5b12b7cb61fb55d406
Oskar Gester <gester.oskar@gmail.com>
2025-04-30 11:01:28 +0200

    finished orderController

---
+++ src/main/java/se/java/security/controllers/OrderController.java
@@ -0,0 +1,44 @@
+package se.java.security.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import se.java.security.dto.OrderDTO;
+import se.java.security.dto.OrderResponse;
+import se.java.security.dto.OrderResponseDTO;
+import se.java.security.services.OrderService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    PostMapping
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<OrderResponseDTO> createOrder(@RequestBody OrderDTO orderDTO) {
+        OrderResponseDTO newOrder = orderService.createOrder(orderDTO);
+        return new ResponseEntity<>(newOrder, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/all")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<OrderResponse>> getAllOrders() {
+        List<OrderResponse> orders = orderService.getAllOrders();
+        return ResponseEntity.ok(orders);
+    }
+
+    @GetMapping("/user/{userId}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<List<OrderResponse>> getUserOrders(@PathVariable String userId) {
+        List<OrderResponse> orders = orderService.getUserOrders(userId);
+        return ResponseEntity.ok(orders);
+    }
+}
